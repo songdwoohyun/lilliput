002b6f9c6c1d80825be6c14c737657bda6bb79f6
songwoohyun <song.d.woohyun@gmail.com>
2026-08-02 18:27:43 +0900

    Initial Vite + React + Tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-[#4a5943] flex items-center justify-center">
+      <h1 className="text-4xl font-serif text-[#f0e4d3]">The Lilliput Collection</h1>
+    </div>
+  )
+}
+
+export default App
